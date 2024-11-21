9115ea988585626b69b29aac598bf33c8d56f1d6
mdh20030408 <1449708797@qq.com>
2024-11-21 21:31:12 +0800

    数据类型

---
+++ js/js_type/2.js
@@ -0,0 +1,14 @@
+let obj={
+    name:'daoge',
+    job:'forntend master',
+    company:'tiktok'
+}
+obj.hometown = "南昌"
+
+let a = 1;
+let b = a;
+b=3;
+ let obj2 = obj;
+ obj2.name ="xiaoqinlin"
+ console.log(a,b)
+ console.log(obj,obj2)
---
+++ js/js_type/3.js
@@ -0,0 +1,3 @@
+// Symbol es6 的新数据类型 独一无二 标签
+// 函数一样的外观，简单数据类型
+console.log(Symbol('马斯克') === Symbol('马斯克'))
