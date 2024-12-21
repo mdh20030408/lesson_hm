cd82f295056fa6343525eb34447ddd1543fdfb52
mdh20030408 <1449708797@qq.com>
2024-12-21 17:01:05 +0800

    vue

--- vue_react/cli/try-vue/src/App.vue
+++ vue_react/cli/try-vue/src/App.vue
@@ -2,8 +2,10 @@
 <div>
 <h1>Hello Vue!</h1>
 <Counter />
+<Repos />
 </div>
 </template>
 <script setup>
+import Repos from './components/Repos.vue'
 import Counter from './components/Counter.vue'
 </script>
