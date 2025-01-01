98f98a812263aac1cbfdd0906d397ea44e87d0eb
mdh20030408 <1449708797@qq.com>
2025-01-01 12:03:41 +0800

    评分

---
+++ vue_react/rate/src/components/AppAside.vue
@@ -0,0 +1,20 @@
+<template>
+    <aside class="flx">
+     <User />
+     <User />
+     <User />
+    </aside>
+</template>
+
+<script setup>
+import User from './User.vue'
+</script>
+
+<style  scoped>
+aside{
+    width: 200px;
+    background-color: green;
+    height: 500px;
+
+}
+</style>
---
+++ vue_react/rate/src/components/Item.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+     Item
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+div{
+    height: 100px;
+    margin: 10px;
+    background-color: grey;
+}
+</style>
