af9ec19cf05c36af2ff17ef00a1d93d89e6ca057
mdh20030408 <1449708797@qq.com>
2025-01-03 21:27:48 +0800

    router

---
+++ vue_react/vue-router-demo/src/views/post/show/post-show.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <div>PostShow {{ postId }}</div>
+    </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+const route = useRoute() // 当前路由
+// console.log(route)
+const {postId} = route.params // 路由参数对象
+</script>
+
+<style lang="scss" scoped>
+
+</style>
