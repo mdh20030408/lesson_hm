aeb9f7d152ac9ce65d76b2422908acbee8b61f15
mdh20030408 <1449708797@qq.com>
2025-01-10 16:57:23 +0800

    引入pinia和stylus,css

--- vue_react/v3-element/src/main.js
+++ vue_react/v3-element/src/main.js
@@ -6,8 +6,10 @@ import {
 } from 'element3'
 import 'element3/lib/theme-chalk/index.css'
 import router from './router'
+import {createPinia} from 'pinia'
 
 createApp(App)
+.use(createPinia())
 .use(router)
 .use(ElButton)
 .mount('#app')
