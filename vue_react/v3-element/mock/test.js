e97f6ac283ec8bdf5354999cae8fd71fab40004d
mdh20030408 <1449708797@qq.com>
2025-01-15 21:36:15 +0800

    mock,axios.jwt

---
+++ vue_react/v3-element/mock/test.js
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken'
+const {sign} = jwt;
+// 密钥
+const secret= 'g10bqwer123';
+
+export default [
+    {
+      url: '/api/login',
+      method: 'post',
+      timeout: 2000,
+      response: (req, res) => {
+        let body = req.body
+        // console.log(body)
+        if(body.name !== 'admin' || body.password !== '123456'){
+          return {
+            code: 60204,
+            message: 'Usename or password are incorrect'
+          }
+        }
+
+        // 签发令牌 token
+         const token =sign({user:body.name},secret,{
+          expiresIn:60 * 60 * 24 * 7
+        })
+
+        return {
+          code: 200,
+          data:token
+        }
+      }
+    } 
+  ]
--- vue_react/v3-element/vite.config.js
+++ vue_react/v3-element/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import { viteMockServe } from 'vite-plugin-mock'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),viteMockServe({
+    mockPath: 'mock',
+    localEnabled:true,
+  })],
 })
