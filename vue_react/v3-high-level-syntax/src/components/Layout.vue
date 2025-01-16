e073e20c27e5ee442d94202512f1f9ef0aa05f9f
mdh20030408 <1449708797@qq.com>
2025-01-16 21:30:45 +0800

    插槽,watcheffect

---
+++ vue_react/v3-high-level-syntax/src/components/Layout.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+    <h1>Layout 组件</h1>
+    <slot></slot>
+    <header>
+        <slot name="header"></slot>
+    </header>
+    <main>
+        <slot></slot>
+    </main>
+    <footer>
+        <slot name="footer"></slot>
+    </footer>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
