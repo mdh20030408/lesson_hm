8bbecde220b5f466c5244a3e239865ebad440845
mdh20030408 <1449708797@qq.com>
2025-01-23 20:17:04 +0800

    tailwind

---
+++ tailwindcss/tailwind-demo/postcss.config.js
@@ -0,0 +1,10 @@
+// postcss.config.mjs
+import tailwind from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+export default {
+  plugins: [
+    tailwind(),
+    autoprefixer(),
+  ],
+};
